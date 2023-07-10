97762b60a41c56d48c47ef84e63656b479bcecf0
Rafael Deluna <delunarafael@yahoo.com>
2023-07-10 04:24:39 -0400

    Completed Sprint 6 Challenge

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
+import './App.css';
+import axios from 'axios';
+import Character from './components/Character';
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -7,12 +11,51 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [character,setCharacter] = useState([]);
+  const [currentCharacterName,setCurrentCharacterName] = useState(null);
+
+  const openDetails = (name) => {
+    setCurrentCharacterName(name);
+  }
+
+  const closeDetails = () => {
+    setCurrentCharacterName(null); 
+  }
+  
+
+  useEffect(() => {
+    const fetchCharacters = () => {
+    axios
+       .get('https://swapi.dev/api/people/')
+       .then(res => {
+        console.log(res.data);
+          setCharacter(res.data);
+       })
+       .catch((error) => {
+        debugger;
+       })
+      }
+      fetchCharacters();
+  },[])
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">REACT WARS</h1>
+      {character.map((pr) => {
+        return (
+          <Character
+            key={pr.name}
+            info={pr}
+            character={character}
+            openDetails={openDetails}
+          />
+        );
+      })}
     </div>
   );
-}
+};
+
+
+ 
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,66 @@
 // Write your Character component here
+import React from 'react';
+import styled from 'styled-components';
+
+
+const StyledChar = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  h1 {
+    color: ${(pr) => pr.theme.primaryColor};
+    background-color: ${(pr) => pr.theme.thirdColor};
+    width: 43%;
+    height: 10vh;
+    border: 3px solid black;
+    text-shadow: 1px 1px 5px white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  button {
+    width: 10%;
+    height: 8vh;
+    font-size: 1.3rem;
+    color: ${(pr) => pr.theme.thirdColor};
+
+    background-color: ${(pr) => pr.theme.primaryColor};
+  }
+  ul {
+    color: black;
+    background-color: ${(pr) => pr.theme.thirdColor};
+    width: 40%;
+    border: 3px solid black;
+    align-items: flex-start;
+    font-size: 1.5rem;
+    margin-right: 0px;
+  }
+  p {
+    display: flex;
+  }
+  h3 {
+    display: flex;
+  }
+`;
+
+const Character = (props) => {
+       console.log(props);
+    const { info, openDetails } = props;
+  
+    return (
+      <StyledChar className = "starWars">
+        <h1>{props.info.name} {props.info.birth_year}</h1>
+       
+          
+  
+          {/* <button onClick={() => openDetails(info.name)}>See details</button> */}
+  
+        {/* {props.info.name} */}
+      </StyledChar>
+    );
+  };
+  
+  export default Character;
